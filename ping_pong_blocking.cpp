ed5d328bde954c854482b2af4efbbbe6365dc4f0
Chris Gerlach <gerlac37@css-095.i>
2024-03-05 11:24:49 -0500

    update ping pong blocking

--- ping_pong_blocking.cpp
+++ ping_pong_blocking.cpp
@@ -12,32 +12,30 @@ int main() {
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    int message = 1;
     int maxIterations = 100;
-
     int messageSize = 2;
-    char* buffer = new char[messageSize];
+    char* message = new char[messageSize];
 
     for (int iteration=0; iteration < maxIterations; iteration++) {
     
         if (world_rank == 0) {
             
             // Ping
-            MPI_Send(&buffer, 1, MPI_UNSIGNED_CHAR, 1, 0, MPI_COMM_WORLD);
+            MPI_Send(&message, 1, MPI_UNSIGNED_CHAR, 1, 0, MPI_COMM_WORLD);
             printf("Process 0 Sent Message %d To Process 1\n", message);
 
             // 2nd Pong
-            MPI_Recv(&buffer, 1, MPI_UNSIGNED_CHAR, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&message, 1, MPI_UNSIGNED_CHAR, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("Process 0 Received Message %d From Process 1\n", message);
         } 
         else if (world_rank == 1) {
             
             // Pong
-            MPI_Recv(&buffer, 1, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&message, 1, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("Process 1 Received Message %d From Process 0\n", message);
 
             // 2nd Ping
-            MPI_Send(&buffer, 1, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD);
+            MPI_Send(&message, 1, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD);
 
             printf("Process 1 Sent Message %d To Process 0\n", message);
         }
