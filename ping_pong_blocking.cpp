2734ca3fe677867d2f725d4583fbfcad5abc7d43
Chris Gerlach <gerlac37@css-099.i>
2024-03-07 11:30:38 -0500

    message size command line arg

--- ping_pong_blocking.cpp
+++ ping_pong_blocking.cpp
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s messageSize\n", argv[0]);
+        return 1;
+    }
+
+    int messageSize = atoi(argv[1]);
+    if (messageSize <= 0) {
+        fprintf(stderr, "messageSize must be a positive integer\n");
+        return 1;
+    }
 
     double startTime = MPI_Wtime();
 
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
 
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
@@ -13,7 +25,6 @@ int main() {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     int maxIterations = 100;
-    int messageSize = 2;
     char* message = new char[messageSize];
 
     for (int iteration=0; iteration < maxIterations; iteration++) {
@@ -21,27 +32,28 @@ int main() {
         if (world_rank == 0) {
             
             // Ping
-            MPI_Send(&message, 1, MPI_UNSIGNED_CHAR, 1, 0, MPI_COMM_WORLD);
-            printf("Process 0 Sent Message %d To Process 1\n", message);
+            MPI_Send(message, messageSize, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+            printf("Process 0 Sent Message To Process 1\n");
 
             // 2nd Pong
-            MPI_Recv(&message, 1, MPI_UNSIGNED_CHAR, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Process 0 Received Message %d From Process 1\n", message);
+            MPI_Recv(message, messageSize, MPI_CHAR, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Process 0 Received Message From Process 1\n");
         } 
         else if (world_rank == 1) {
             
             // Pong
-            MPI_Recv(&message, 1, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Process 1 Received Message %d From Process 0\n", message);
+            MPI_Recv(message, messageSize, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Process 1 Received Message From Process 0\n");
 
             // 2nd Ping
-            MPI_Send(&message, 1, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD);
-
-            printf("Process 1 Sent Message %d To Process 0\n", message);
+            MPI_Send(message, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+            printf("Process 1 Sent Message To Process 0\n");
         }
     }   
     
     MPI_Finalize();
+
+    delete[] message; // Free the allocated memory
 
     double endTime = MPI_Wtime();
     double totalTime = endTime - startTime;
@@ -50,4 +62,5 @@ int main() {
         printf("\nTotal Time: %lf \n", totalTime);
     }
 
+    return 0;
 }
