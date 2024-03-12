867353221ef6b0b79bedeedd8d80c42cde39af49
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2024-03-12 11:36:56 -0400

    Made changes

--- ring_shift_blocking.cpp
+++ ring_shift_blocking.cpp
@@ -38,26 +38,26 @@ int main(int argc, char* argv[]) {
             // Rank 0 sends first and then receives
 
             // Send message to the next rank (1)
-            MPI_Send(&message, messageSize, MPI_UNSIGNED_CHAR, world_rank + 1, 0, MPI_COMM_WORLD);
+            MPI_Send(message, messageSize, MPI_UNSIGNED_CHAR, world_rank + 1, 0, MPI_COMM_WORLD);
             // printf("Process 0 Sent Message %p To Process 1\n", message);
 
             // Receives message from the last rank, completing the ring
-            MPI_Recv(&message, messageSize, MPI_UNSIGNED_CHAR, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(recbuf, messageSize, MPI_UNSIGNED_CHAR, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             // printf("Process 0 Received Message %d From Process %d\n", message, world_size-1);
         } else {
             // All the other ranks receive first and then send
             
             // Receieves message from the previous rank
-            MPI_Recv(&message, messageSize, MPI_UNSIGNED_CHAR, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(recbuf, messageSize, MPI_UNSIGNED_CHAR, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             // printf("Process %d Received Message %d From Process %d\n", world_rank, message, world_rank - 1);
 
             if (world_rank == world_size - 1) {
                 // Final rank should send message to rank 0, completing the ring
-                MPI_Send(&message, messageSize, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
+                MPI_Send(message, messageSize, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
                 // printf("Process %d Sent Message %d To Process %d\n", world_rank, message, 0);
             } else {
                 // All the other ranks send message to the next rank 
-                MPI_Send(&message, messageSize, MPI_UNSIGNED_CHAR, world_rank + 1, 0, MPI_COMM_WORLD);
+                MPI_Send(message, messageSize, MPI_UNSIGNED_CHAR, world_rank + 1, 0, MPI_COMM_WORLD);
                 // printf("Process %d Sent Message %d To Process %d\n", world_rank, message, world_rank + 1);
             }
         }
