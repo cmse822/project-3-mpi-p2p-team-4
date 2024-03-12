6da185e24761c83477afd78526db883bfa57d234
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2024-03-12 09:38:24 -0400

    Added arguments and writing to csv file

--- ring_shift_blocking.cpp
+++ ring_shift_blocking.cpp
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main() {
-    MPI_Init(NULL, NULL);
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s messageSize\n", argv[0]);
+        return 1;
+    }
+
+    int messageSize = atoi(argv[1]);
+
+    printf("message size %lld \n", messageSize);
+    
+    if (messageSize <= 0) {
+        fprintf(stderr, "messageSize must be a positive integer\n");
+        return 1;
+    }
 
     double startTime = MPI_Wtime();
-    
+
+    MPI_Init(&argc, &argv);
+
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     int maxIterations = 100;
-    int messageSize = 2;
-    char* message = new char[messageSize];
+
+    char* message = (char*)malloc(messageSize);
+    char* recbuf = (char*)malloc(messageSize);
+
+    const char* csvfile = argv[2];
 
     for (int iteration=0; iteration < maxIterations; iteration++) {
         if (world_rank == 0) {
@@ -50,8 +67,30 @@ int main() {
     double endTime = MPI_Wtime();
     double totalTime = endTime - startTime;
 
-    if (world_rank == 0) {
+    if (world_rank == 0 ) {
         printf("\nTotal Time: %lf \n", totalTime);
-    }
 
+        // const char* csvfile = "non_blocking.csv";
+
+        std::ifstream infile(csvfile);
+         //check if file is empty
+        bool is_empty = infile.peek() == std::ifstream::traits_type::eof();
+        infile.close();
+
+        // Open the CSV file for writing (appending mode)
+        std::ofstream file(csvfile, std::ios_base::app); // app = append mode
+
+        // Write header if the file is empty
+        if (is_empty) {
+            file << "processors, message size, Time" << std::endl;
+        }
+
+        // Write results to the CSV file
+        if (file.is_open()) {
+            file << world_size << ',' << messageSize << ',' << totalTime << std::endl;
+            file.close();
+        } else {
+            std::cerr << "Error: Unable to open file for writing." << std::endl;
+        }
+    }
 }
